2c6f403790664de835348ce2610957682fd70eed
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-08-25 21:48:50 +0530

    Introduction

---
+++ Packages.r
@@ -0,0 +1,5 @@
+# LOAD PACKAGES ###########################
+
+# "pacman" is recommended for managing add-on packages.
+# It will install packages, if needed, and then load the packages.
+install.packages("pacman")
