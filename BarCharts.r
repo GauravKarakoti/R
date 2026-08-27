6ea877ae819bf5a436aa5d0ac31194b834f3daeb
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-08-27 13:58:33 +0530

    Bar Charts

---
+++ BarCharts.r
@@ -0,0 +1,22 @@
+# LOAD DATASETS PACKAGES ####################################
+
+library(datasets)
+
+# LOAD DATA #################################################
+
+?mtcars
+head(mtcars)
+
+# BAR CHARTS ################################################
+
+barplot(mtcars$cyl)    # Doesn't work
+
+# Need a table with frequencies for each category
+cylinders <- table(mtcars$cyl)    # Create table
+barplot(cylinders)                # Bar chart
+plot(cylinders)                   # Default X-Y plot (lines)
+
+# CLEAN UP ##################################################
+
+# Clear environment
+rm(list=ls())
