8242321ba987f1d9f9e270e74f6b435d2f4f314e
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-09-08 14:49:15 +0530

    Hierarchical Clustering

---
+++ HierarchicalClustering.r
@@ -0,0 +1,39 @@
+# INSTALL AND LOAD PACKAGES ####################
+
+# Packages to load; uses "pacman"
+pacman::p_load(
+    pacman, dplyr, GGally, ggplot2, ggthemes,
+    ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny,
+    stringr, tidyr
+)
+
+library(datasets)    # Load base packages manually
+
+# LOAD DATA ####################################
+
+?mtcars
+head(mtcars)
+cars <- mtcars[, c(1:4, 6:7, 9:11)]    # Select variables
+head(cars)
+
+# COMPUTE AND PLOT CLUSTERS ####################
+
+# Save hierarchical clustering to "hc". This code uses
+# pipes from dplyr.
+hc <- cars %>%    # Get cars data
+      dist %>%    # Compute distance/dissimilarity matrix
+      hclust      # Compute hierarchical clusters
+
+plot(hc)    # PLot dendrogram
+
+# ADD BOXES TO PLOT ############################
+
+rect.hclust(hc, k=2, border="gray")
+rect.hclust(hc, k=3, border="blue")
+rect.hclust(hc, k=4, border="green4")
+rect.hclust(hc, k=5, border="darkred")
+
+# CLEAN UP #####################################
+
+# Clear environment
+rm(list=ls())
